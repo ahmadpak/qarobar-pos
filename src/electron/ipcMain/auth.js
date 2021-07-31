59bd19eec53a2c7e52853838d303b15950907654
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-07-31 19:13:37 +0500

    init(vuex)

--- src/electron/ipcMain/auth.js
+++ src/electron/ipcMain/auth.js
@@ -8,11 +8,9 @@ ipcMain.on("login", (event, args) => {
     .then(function (response) {
       const [cookie] = response.headers["set-cookie"]; // get cookie from request
       axios.defaults.headers.Cookie = cookie; // attach cookie to axiosInstance for future requests
-      console.log(response.data);
       event.reply("auth-reply", response.data);
     })
     .catch((error) => {
-      console.log(error.response.data.message);
       event.reply("auth-reply", {
         error: error.response.data.message,
         msg: "msg",

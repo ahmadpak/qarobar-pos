6f1bb0b50ef2ca1dc538fe69211eab2667641b5d
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-07-31 17:04:53 +0500

    init(vuetify)

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+
   render: function (h) {
     return h(App);
   },

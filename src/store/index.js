5e97cb4d801146afb31fadf5908b0c6a8d15f780
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2021-08-01 23:48:15 +0500

    init(vuex)

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,29 @@ export default new Vuex.Store({
     lastErrorMsg: "",
     lastMsg: "",
   },
-  mutations: {},
-  actions: {},
+  // sync calls
+  mutations: {
+    setLoggedIn(state, payload) {
+      state.loggedIn = payload;
+    },
+    setLastErrorMsg(state, payload) {
+      state.lastErrorMsg = payload;
+    },
+    setLastMsg(state, payload) {
+      state.lastMsg = payload;
+    },
+  },
+  // async calls
+  actions: {
+    setLoggedIn(context, payload) {
+      context.loggedIn = payload;
+    },
+    setLastErrorMsg(context, payload) {
+      context.lastErrorMsg = payload;
+    },
+    setLastMsg(context, payload) {
+      context.lastMsg = payload;
+    },
+  },
   modules: {},
 });
---
+++ src/store/mutationTypes.js
@@ -0,0 +1,5 @@
+export default {
+  SetLoggedIn: "setLoggedIn",
+  SetLastErrorMsg: "setLastErrorMsg",
+  SetLastMsg: "setLastMsg",
+};
